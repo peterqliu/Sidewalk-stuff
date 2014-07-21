25fe72e337ba7bdcf42fc5ab130c8387f68aa641
peterqliu <peterqliu@gmail.com>
2014-07-20 17:20:54 -0700

    built circle searcher

---
+++ apartment.js
@@ -0,0 +1,263 @@
+			window.latitude=37.7484;
+			window.longitude=-122.4156;
+			
+			//initialize main map and detail map
+			var map=L.mapbox.map('map', 'peterqliu.j0nb49fm', {
+			    scrollWheelZoom: true, zoomControl:false
+			});
+
+			new L.Control.Zoom({ position: 'topright' }).addTo(map);
+			var detailmap=L.mapbox.map('detailedmap', 'examples.map-0l53fhk2', {scrollWheelZoom: true,zoomControl:false});
+			
+			
+			
+			//fetch data via 3taps
+			window.array;
+			window.searchparameters= {auth_token: "358297d537d9b89bd5b5720bbe255c65", 	
+				//anchor:1137032940,
+				rpp:'99',
+				retvals:'id,images,price,annotations,location,timestamp',
+				 category:"RHFR",
+				 source:'CRAIG',
+				 radius:'8000m',
+				 //heading:'san francisco',
+				 lat: latitude,
+				 long: longitude,
+				 price:''
+				};
+			
+			//search with typed input
+		    function inputsearch()
+				{//translate inputed city/ZIP into lat-lon
+				var locationquery=$('#locationinput').val();
+				geocoderurl='http://api.tiles.mapbox.com/v4/geocode/mapbox.places-v1/'+locationquery+'.json?access_token=pk.eyJ1IjoicGV0ZXJxbGl1IiwiYSI6ImpvZmV0UEEifQ._D4bRmVcGfJvo1wjuOpA1g'
+				$.get(geocoderurl).done(function(results) 
+					{searchparameters['lat']=results['features'][0]['center'][1];
+					searchparameters['long']=results['features'][0]['center'][0];	
+					searchparameters['radius']='10mi';
+					map.setView([searchparameters['lat'],searchparameters['long']], 14);
+
+					dosearch();
+					
+					})
+				};	
+			
+			//search by radiused circle
+			function circlesearch(lat,lon,radius) {
+				searchparameters['lat']=lat;
+				searchparameters['long']=lon;
+				searchparameters['radius']=radius+'m';
+				
+				dosearch();
+			}
+			
+			//execute search with provided parameters
+			function dosearch(){
+				
+				
+				$.get( "http://search.3taps.com", searchparameters)
+					
+					.done(function(data) {
+				console.log('done fetching');
+				
+				//remove current markers
+				$('#map .leaflet-marker-icon').remove();				
+				array=data.postings;
+				
+				//array to store latlons of all markers for map positioning purposes
+				window.markerextent=[];
+					
+				//Place markers on map	
+				for (var k=0;k<array.length;k++)
+					{//define latlon
+					var lat=array[k]['location']['lat']; 
+					var lon=array[k]['location']['long'];
+					
+					//define bedroom number
+					var bedroomquant;
+					switch (array[k]['annotations']['bedrooms']) {
+					  	case "0br":
+					  		bedroomquant='studio';
+					  		break;
+						default:
+							bedroomquant=array[k]['annotations']['bedrooms'];
+					}
+					
+					//set default picture if listing doesn't have one
+					if (array[k]['images'][0] !== undefined && array[k]['images'][0]['full'] !== undefined) {var image=array[k]['images'][0]['full']} 
+					else {var image= 'http://www.peterma.de/secretrio/assets/selfie1.jpg'};
+						
+					var myIcon=L.divIcon({className: 'leaflet-marker-icon',html: '<img src="'+image+'"><div class="markertitle">'+bedroomquant+'<br>$'+array[k]['price']+'</div>'});		
+					L.marker([lat,lon],{icon: myIcon,riseOnHover:'true',listingindex:k })
+							.bindPopup('<a>'+k+'</a>')
+							.addTo(map)
+							.on('click',function(e){toggledetailview(); populatelisting(e['target']['_popup']['_contentNode']['innerText'])})
+					
+					//add latlon to array determining view positioning
+					markerextent[k]=[lat,lon];
+					}
+					
+				//fit map view to active markers
+				map.fitBounds(markerextent);
+				})
+			}	
+					    
+			inputsearch();
+			
+			//open and close detail window
+			function toggledetailview(){$("#detailview").toggleClass("open")};
+			
+			//fetch heading, timestamp, HTML body text
+			function fetchdetail(id){$.get( "http://search.3taps.com", 
+				{auth_token: "358297d537d9b89bd5b5720bbe255c65",id:id,retvals:'html,heading,timestamp,external_url,price'})
+				.done(function(data) 
+					{var html=atob(data.postings[0]['html']);
+					var postingbody=html.substring(html.lastIndexOf('<section id="postingbody">')+26,html.lastIndexOf('<ul class="notices">'));
+					
+					$('#listingtitle').text('$'+data.postings[0]['price']+' '+data.postings[0]['heading']);
+					$('#listingbody').html(postingbody);
+					
+					//update Contact and Listing buttons
+					$('#listinglink').attr('href',data.postings[0]['external_url']);
+	
+					}
+				)};
+			
+			//when user clicks on marker, populates detail view with appropriate heading and body
+			function populatelisting(e)
+				{var listing=array[e];
+				//$('#listingbody').text(listing['body']);
+				fetchdetail(listing['id']);
+				
+				//calculate elapsed time since posting date
+				var currenttime=(new Date().getTime())/1000;
+				var postingtime; 
+					if (listing['annotations']['original_posting_date']!== undefined){postingtime=listing['annotations']['original_posting_date']} 
+					else {postingtime= listing['timestamp']};
+				var difference=currenttime-postingtime;
+
+				if(difference<3600){$('#dateposted').text(Math.round(difference/60)+' minutes ago')}
+				else {if(difference<62400) {$('#dateposted').text(Math.round(difference/3600)+' hours ago')}
+					else {$('#dateposted').text(Math.round(difference/86400)+' days ago')}
+				}
+				//console.log(hours+' hours since this listing was posted');
+				
+				//insert pictures
+				$('#gallery').text('');
+				for(var m=0;m<listing['images'].length;m++)
+					{$('#gallery').append("<img class='galleryimg' src='"+listing['images'][m]['full']+"'>")}
+
+					//center detail map to listing location								
+					var lat=listing['location']['lat']; 
+					var lon=listing['location']['long'];
+				
+				//populate bed/bath, price
+				
+				$('#bedbath').text(listing['annotations']['bedrooms']+', '+listing['annotations']['bathrooms']+' in '+listing['annotations']['source_neighborhood']);
+				
+				//remove old marker, reset view on new listing, insert new marker at location with tooltip containing intersection (via geonames API call)
+				$('#detailedmap .leaflet-marker-icon').remove();
+				detailmap.setView([lat,lon], 16);
+				$.ajax({ url: 'http://api.geonames.org/findNearestIntersectionJSON?lat='+lat+'&lng='+lon+'&username=peterqliu', 
+					success: function(data) { 
+						L.marker([lat,lon],{icon: L.divIcon({className: 'leaflet-marker-icon',html: '<img src="http://www.peterma.de/secretrio/assets/selfie1.jpg">'})})
+						.bindPopup('<div id="intersectiontooltip">'+data['intersection']['street1']+' at '+data['intersection']['street2']+'</div>',{closeButton:'false'})
+						.addTo(detailmap)
+						.openPopup();
+					}
+				})			
+				}
+				
+				
+			var drawing='false';
+
+			function drawcircle()
+				{drawing='true';
+
+					//remove existing circles
+					$('path.leaflet-clickable').remove();
+					
+					//temporarily disable map drag
+					map.dragging.disable();
+					
+					map.on('mousedown', function(e) {
+						//console.log(e.containerPoint.toString() + ', ' + e.latlng.toString());	
+						window.centerlatlon=[e.latlng.lat,e.latlng.lng];
+								
+					});		
+							
+					$('#map').mousedown(function(e)
+						{$('#circledraw').toggle();
+						
+						var circlecenter=[e.pageX,e.pageY];
+						d3.select('#center')
+						.attr('cx',circlecenter[0])
+						.attr('cy',circlecenter[1]);	
+												
+						d3.select('#outer')
+						.attr('cx',circlecenter[0])
+						.attr('cy',circlecenter[1])
+						.attr('r',0);
+						
+						$('#map').mousemove(function(e)
+							{ if(drawing=='true' && e.which==1)
+								{var radius=Math.pow(Math.pow(e.pageX-circlecenter[0],2)+Math.pow(e.pageY-circlecenter[1],2),0.5);
+								d3.select('#outer').attr('r', radius).attr('fill-opacity',Math.pow(0.9,(radius*0.1)));
+								}
+							}
+						)
+						})
+						map.on('mouseup',function(e)
+							{if (drawing=='true')
+								{
+								map.dragging.enable();
+								$('#circledraw').hide();
+								$('#map').unbind('mousedown');
+								var meterradius= L.latLng(centerlatlon[0],centerlatlon[1]).distanceTo([e.latlng.lat,e.latlng.lng]);
+								L.circle(centerlatlon, meterradius).addTo(map);
+								circlesearch(centerlatlon[0],centerlatlon[1],meterradius);
+								}
+								drawing='false';
+							}
+						);
+				}		
+	
+				//Price scrubber functionality
+				  var currentmin=200;
+				  var initialmin;
+				  var currentmax=3000;
+				  var initialmax;
+				  
+				  var minprice=0;
+				  var maxprice=10000;
+				  
+				  var minactive;
+				  var maxactive;
+				  
+				  $('#minrent').on('mousedown',function(e)
+				  {minactive=true; var initialx=e.pageX; initialmin=currentmin;
+						$('body').mousemove(function(e)
+					  	{window.xdrag=Math.round(e.pageX-initialx)*10;
+						if (minactive && xdrag%10==0 && initialmin+xdrag>minprice && initialmin+xdrag<currentmax)
+							{currentmin=initialmin+xdrag;
+							$('#minrent').text('$'+currentmin);}
+				        }
+				      )
+				    }
+				  );
+				
+				  $('#maxrent').on('mousedown',function(e)
+				    {maxactive=true; var initialx=e.pageX; initialmax=currentmax;
+				    	
+				      $('body').mousemove(function(e)
+				        {window.xdrag=Math.round(e.pageX-initialx)*10;
+				        if (maxactive && xdrag%10==0 && initialmax+xdrag>currentmin && initialmax+xdrag<maxprice)
+				        	{currentmax=initialmax+xdrag;
+				        	$('#maxrent').text('$'+currentmax);}
+				        }     
+				      )
+				    }
+				  ).mouseup()
+				  $('body').on('mouseup', function() 
+				  {if (minactive==true || maxactive==true)
+				  {searchparameters['price']=currentmin+'..'+currentmax;dosearch();minactive=false;maxactive=false;}})				
